ab0ece7ba3927f47a6639fbcda572b0d997c1ed7
Krupesh Anadkat <kmanadkat@gmail.com>
2021-10-15 07:42:15 +0530

    Added Dotenv Webpack Support

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,15 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "eval-cheap-source-map",
+  plugins: [
+    new Dotenv({
+      path: "./.env.development",
+    }),
+  ],
   devServer: {
     port: 3000,
   },
